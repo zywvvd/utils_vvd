4e5fd59731f0701499bec5c1a25ebf6b39e11be7
VVD <zywvvd@mail.ustc.edu.cn>
2021-10-14 16:13:38 +0800

    update-resize

--- image_processing.py
+++ image_processing.py
@@ -33,7 +33,7 @@ def show_hist(img):
     return histogram
 
 
-def image_resize(img_source, shape=None, factor=None, unique_check=False):
+def image_resize(img_source, shape=None, factor=None, unique_check=False, interpolation=None):
     image_H, image_W = img_source.shape[:2]
     if shape is not None:
         resized_image = cv2.resize(img_source, shape)
@@ -48,7 +48,7 @@ def image_resize(img_source, shape=None, factor=None, unique_check=False):
         resized_H = int(round(image_H * factor_y))
         resized_W = int(round(image_W * factor_x))
 
-        resized_image = cv2.resize(img_source, [resized_W, resized_H])
+        resized_image = cv2.resize(img_source, [resized_W, resized_H], interpolation=interpolation)
 
     elif shape is None and factor is None:
         resized_image = img_source
